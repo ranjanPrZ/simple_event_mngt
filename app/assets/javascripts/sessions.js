fc231246380bcf931605e67697a8da44ecdff8c9
Ranjan <ranjantabby@gmail.com>
2021-05-01 20:01:18 +0530

    Add validation for session log

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("form").submit(function(){
+		var email_field = $("#email").val();
+		var pwd_field = $("#password").val();
+		if(email_field.trim().length == 0){;
+			$("#name_error").hide()
+			$("#email_error").text("Enter email").show();
+			return false;
+		}else if(pwd_field.trim().length == 0){
+			$("#name_error, #email_error").hide()
+			$("#pwd_error").text("Enter password").show();
+			return false;
+		}
+	})
+})
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,13 +7,16 @@ $(document).ready(function(){
 		if(name_field.trim().length == 0){
 			$("#name_error").text("Enter name").show();
 			return false;
-		}else if(email_field.trim().length == 0){
+		}else if(email_field.trim().length == 0){;
+			$("#name_error").hide()
 			$("#email_error").text("Enter email").show();
 			return false;
 		}else if(pwd_field.trim().length == 0){
+			$("#name_error, #email_error").hide()
 			$("#pwd_error").text("Enter password").show();
 			return false;
 		}else if(pwd_cnfrm_field.trim().length == 0){
+			$("#name_error, #email_error, #pwd_error").hide()
 			$("#cmf_pwd_error").text("Enter Confirmation password").show();
 			return false;
 		}
